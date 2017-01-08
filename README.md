bd160f0e563943838ce67d51134725b3f64d3e9a
Bryan Baker <bryanpbaker@gmail.com>
2017-01-08 11:51:17 -0600

    init commit

---
+++ README.md
@@ -0,0 +1,11 @@
+<h1>TOM Frontend</h1>
+
+<h2>Front end application for the TOM app. Built with React/Redux. Built to be separate from backend.</h2>
+
+<h2>Getting Started</h2>
+<ul>
+    <li>Clone this repo</li>
+    <li><code>cd tom-frontend</code></li>
+    <li><code>npm install</code></li>
+    <li><code>npm start</code></li>
+</ul>
---
+++ src/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Router, Route, Link, browserHistory, IndexRoute} from 'react-router';
+
+import Header from './components/header.component';
+import Test from './components/test.component';
+
+import '../style/main.scss';
+
+const App = (props) => {
+
+    return(
+        <div className="app row">
+            <Header/>
+            {props.children}
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <Router history={browserHistory}>
+        <Route path="/" component={App}>
+            <Route path="test" component={Test}></Route>
+        </Route>
+    </Router>
+, document.getElementById('app'));
